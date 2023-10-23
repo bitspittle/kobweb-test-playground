27e6e46d9c064e6f76c81d188a2e362329e907ab
David Herman <bitspittle@gmail.com>
2023-10-23 12:14:03 -0700

    Export with switch

--- site/src/jsMain/kotlin/org/example/pages/Index.kt
+++ site/src/jsMain/kotlin/org/example/pages/Index.kt
@@ -1,6 +1,6 @@
 package org.example.pages
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import com.varabyte.kobweb.compose.css.Isolation
 import com.varabyte.kobweb.compose.css.StyleVariable
 import com.varabyte.kobweb.compose.css.functions.blur
@@ -18,6 +18,8 @@ import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.core.rememberPageContext
 import com.varabyte.kobweb.silk.components.forms.Button
+import com.varabyte.kobweb.silk.components.forms.Switch
+import com.varabyte.kobweb.silk.components.forms.SwitchSize
 import com.varabyte.kobweb.silk.components.layout.breakpoint.displayIfAtLeast
 import com.varabyte.kobweb.silk.components.navigation.Link
 import com.varabyte.kobweb.silk.components.style.ComponentStyle
@@ -36,116 +38,68 @@ import org.example.SubheadlineTextStyle
 import org.example.components.layouts.PageLayout
 import org.example.toSitePalette
 
-// Container that has a tagline and grid on desktop, and just the tagline on mobile
-val HeroContainerStyle by ComponentStyle {
-    base { Modifier.fillMaxWidth().gap(2.cssRem) }
-    Breakpoint.MD { Modifier.margin { top(20.vh) } }
-}
-
-// A demo grid that appears on the homepage because it looks good
-val HomeGridStyle by ComponentStyle.base(extraModifiers = Modifier.displayIfAtLeast(Breakpoint.MD)) {
-    Modifier
-        .grid {
-            rows { repeat(3) { size(1.fr) } }
-            columns { repeat(5) { size(1.fr) } }
-        }
-        .gap(0.5.cssRem)
-        .width(70.cssRem)
-        .height(18.cssRem)
-}
-
-private val GridCellColorVar by StyleVariable<Color>()
-val HomeGridCellStyle by ComponentStyle.base {
-    Modifier
-        .backgroundColor(GridCellColorVar.value())
-        .boxShadow(blurRadius = 0.6.cssRem, color = GridCellColorVar.value())
-        .borderRadius(1.cssRem)
-}
-
-@Composable
-private fun GridCell(color: Color, row: Int, column: Int, width: Int? = null, height: Int? = null) {
-    Div(
-        HomeGridCellStyle.toModifier()
-            .setVariable(GridCellColorVar, color)
-            .gridItem(row, column, width, height)
-            .toAttrs()
-    )
-}
-
-@Composable
-private fun SvgBlurredEllipse(color: Color, modifier: Modifier) {
-    // Set max width to 100% so that on really small mobile screens, the ellipse will start to shrink and
-    // avoid causing a horizontal scrollbar to appear
-    Svg(attrs = modifier.filter(blur(100.px)).maxWidth(100.percent).toAttrs()) {
-        Ellipse {
-            cx(50.percent)
-            cy(50.percent)
-            rx(50.percent)
-            ry(50.percent)
-            fill(color)
-        }
-    }
-}
-
 @Page
 @Composable
 fun HomePage() {
     PageLayout("Home") {
-        Row(HeroContainerStyle.toModifier()) {
-            Box {
-                val sitePalette = ColorMode.current.toSitePalette()
+        var selected by remember { mutableStateOf(false) }
+        Switch(selected, onCheckedChange = { selected = it }, size = SwitchSize.LG)
 
-                SvgBlurredEllipse(sitePalette.brand.accent, Modifier.align(Alignment.TopEnd))
-                SvgBlurredEllipse(sitePalette.brand.primary, Modifier.align(Alignment.BottomStart))
-
-                // The Isolate modifier is used to prevent the blurred ellipses from affecting these elements
-                // It's weird, but that's CSS! Read up on stacking contexts for more information.
-                Column(Modifier.gap(2.cssRem).isolation(Isolation.Isolate)) {
-                    Div(HeadlineTextStyle.toAttrs()) {
-                        SpanText(
-                            "Use this template as your starting point for ", Modifier.color(
-                                when (ColorMode.current) {
-                                    ColorMode.LIGHT -> Colors.Black
-                                    ColorMode.DARK -> Colors.White
-                                }
-                            )
-                        )
-                        SpanText(
-                            "Kobweb",
-                            Modifier
-                                .color(sitePalette.brand.accent)
-                                // Use a shadow so this light-colored word is more visible in light mode
-                                .textShadow(0.px, 0.px, blurRadius = 0.5.cssRem, color = Colors.Gray)
-                        )
-                    }
-
-                    Div(SubheadlineTextStyle.toAttrs()) {
-                        SpanText("You can read the ")
-                        Link("/about", "About")
-                        SpanText(" page for more information.")
-                    }
-
-                    val ctx = rememberPageContext()
-                    Button(onClick = {
-                        // Change this click handler with your call-to-action behavior
-                        // here. Link to an order page? Open a calendar UI? Play a movie?
-                        // Up to you!
-                        ctx.router.tryRoutingTo("/about")
-                    }, colorScheme = ColorSchemes.Blue) {
-                        Text("This could be your CTA")
-                    }
-                }
-            }
-
-            Div(HomeGridStyle.toAttrs()) {
-                val sitePalette = ColorMode.current.toSitePalette()
-                GridCell(sitePalette.brand.primary, 1, 1, 2, 2)
-                GridCell(ColorSchemes.Monochrome._600, 1, 3)
-                GridCell(ColorSchemes.Monochrome._100, 1, 4, width = 2)
-                GridCell(sitePalette.brand.accent, 2, 3, width = 2)
-                GridCell(ColorSchemes.Monochrome._300, 2, 5)
-                GridCell(ColorSchemes.Monochrome._800, 3, 1, width = 5)
-            }
-        }
+//        Row(HeroContainerStyle.toModifier()) {
+//            Box {
+//                val sitePalette = ColorMode.current.toSitePalette()
+//
+//                SvgBlurredEllipse(sitePalette.brand.accent, Modifier.align(Alignment.TopEnd))
+//                SvgBlurredEllipse(sitePalette.brand.primary, Modifier.align(Alignment.BottomStart))
+//
+//                // The Isolate modifier is used to prevent the blurred ellipses from affecting these elements
+//                // It's weird, but that's CSS! Read up on stacking contexts for more information.
+//                Column(Modifier.gap(2.cssRem).isolation(Isolation.Isolate)) {
+//                    Div(HeadlineTextStyle.toAttrs()) {
+//                        SpanText(
+//                            "Use this template as your starting point for ", Modifier.color(
+//                                when (ColorMode.current) {
+//                                    ColorMode.LIGHT -> Colors.Black
+//                                    ColorMode.DARK -> Colors.White
+//                                }
+//                            )
+//                        )
+//                        SpanText(
+//                            "Kobweb",
+//                            Modifier
+//                                .color(sitePalette.brand.accent)
+//                                // Use a shadow so this light-colored word is more visible in light mode
+//                                .textShadow(0.px, 0.px, blurRadius = 0.5.cssRem, color = Colors.Gray)
+//                        )
+//                    }
+//
+//                    Div(SubheadlineTextStyle.toAttrs()) {
+//                        SpanText("You can read the ")
+//                        Link("/about", "About")
+//                        SpanText(" page for more information.")
+//                    }
+//
+//                    val ctx = rememberPageContext()
+//                    Button(onClick = {
+//                        // Change this click handler with your call-to-action behavior
+//                        // here. Link to an order page? Open a calendar UI? Play a movie?
+//                        // Up to you!
+//                        ctx.router.tryRoutingTo("/about")
+//                    }, colorScheme = ColorSchemes.Blue) {
+//                        Text("This could be your CTA")
+//                    }
+//                }
+//            }
+//
+//            Div(HomeGridStyle.toAttrs()) {
+//                val sitePalette = ColorMode.current.toSitePalette()
+//                GridCell(sitePalette.brand.primary, 1, 1, 2, 2)
+//                GridCell(ColorSchemes.Monochrome._600, 1, 3)
+//                GridCell(ColorSchemes.Monochrome._100, 1, 4, width = 2)
+//                GridCell(sitePalette.brand.accent, 2, 3, width = 2)
+//                GridCell(ColorSchemes.Monochrome._300, 2, 5)
+//                GridCell(ColorSchemes.Monochrome._800, 3, 1, width = 5)
+//            }
+//        }
     }
 }
